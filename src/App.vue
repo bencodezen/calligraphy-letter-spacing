77f3cf264d55d290220776b200940de73983dfab
Ben Hong <ben@bencodezen.io>
2021-05-07 17:18:34 -0400

    feature: add styles to typography

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <main class="wrapper">
-    <div id="letter" class="test" ref="letter">
+    <div id="letter" class="a6" ref="letter">
       <div class="line-1">{{ letterState.text }}</div>
+      <div class="line-2">1000 Hilltop Circle</div>
+      <div class="line-3">Baltimore, MD</div>
+      <div class="line-4">20852</div>
     </div>
     <p>{{ letterState }}</p>
     <input type="text" v-model="letterState.text" />
@@ -17,7 +20,8 @@ export default {
 
     const letterState = reactive({
       offsetLeft: 0,
-      width: 0
+      width: 0,
+      text: ''
     })
 
     watchEffect(() => {
@@ -49,12 +53,40 @@ export default {
 }
 
 #letter {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   border: 2px solid black;
-  text-align: center;
+}
+
+.line-1,
+.line-3,
+.line-4 {
+  display: inline-block;
+  font-family: 'Dancing Script', cursive;
+  font-weight: 800;
 }
 
 .line-1 {
+  font-size: 3.75rem;
+}
+
+.line-2 {
   display: inline-block;
+  font-family: 'Noto Sans JP', sans-serif;
+  font-size: 1.75rem;
+  margin-top: -5px;
+  text-transform: uppercase;
+}
+
+.line-3,
+.line-4 {
+  font-size: 2rem;
+}
+
+.line-4 {
+  letter-spacing: 10px;
 }
 
 .test {
@@ -63,7 +95,7 @@ export default {
 }
 
 .a6 {
-  width: 4.75in;
-  height: 6.5in;
+  height: 4.75in;
+  width: 6.5in;
 }
 </style>
