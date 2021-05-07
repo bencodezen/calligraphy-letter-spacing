6eea6b4139a50ede00cbaaaddafb68f9339bb467
Ben Hong <ben@bencodezen.io>
2021-05-07 16:28:19 -0400

    feature: add service worker

--- vue.config.js
+++ vue.config.js
@@ -5,13 +5,10 @@ module.exports = {
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js'
-      // ...other Workbox options...
+      swSrc: 'src/serviceWorker.js'
     }
   }
 }
